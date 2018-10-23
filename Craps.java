22789a484ad8c0f8085a18fc526d499719cd8e0b
khristova21 <42977135+khristova21@users.noreply.github.com>
2018-10-23 13:32:21 -0400

    Add files via upload

---
+++ Craps.java
@@ -0,0 +1,38 @@
+/**
+ * @author Krisi Hristova
+ * Craps Game
+ * Pd. 6
+ *
+ *FOR FIRST ROLL
+ *7 and 11 rolled = win
+ *2 or 12 rolled = loose
+ *anything else becomes YOUR point
+ *
+ *if no win or loose
+ *	roll until you get YOUR POINT
+ *	if 7 = lose
+ */
+//"press enter to roll"
+//in.nextLine
+import java.util.Scanner;
+public class Craps 
+{
+
+	public static void main(String []args)
+	{
+		Scanner in = new Scanner(System.in);
+		//2 die
+		Dice die1 = new Dice();
+		Dice die2 = new Dice();
+		
+		System.out.print("Hello! Welcome to Craps!!! Press enter to roll your two dies");
+		in.nextLine();
+		
+		int roll1 = die1.roll();
+		int roll2 = die2.roll();
+		int sum = roll1 + roll2;
+		
+		
+		
+	}
+}
