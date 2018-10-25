0360fd04a5417cb781aa51f881bdaebb782abca0
khristova21 <42977135+khristova21@users.noreply.github.com>
2018-10-25 13:34:00 -0400

    Add files via upload

--- Craps.java
+++ Craps.java
@@ -12,8 +12,6 @@
  *	roll until you get YOUR POINT
  *	if 7 = lose
  */
-//"press enter to roll"
-//in.nextLine
 import java.util.Scanner;
 public class Craps 
 {
@@ -35,7 +33,7 @@ public class Craps
 			int roll1 = die1.roll();
 			int roll2 = die2.roll();
 			int sum = roll1 + roll2;
-			int myPoint = sum;
+			int myPoint;
 
 			System.out.print("Die  1: " + roll1 +  "\tDie 2: " + roll2 + "\tSum: " + sum);
 
@@ -43,60 +41,65 @@ public class Craps
 			//win on first roll
 			if (sum == 7 || sum == 11)
 			{
-				System.out.print("You win! Would you like to play again? (y/n)");
+				System.out.print("\nYou win!");
 				
 			}
 			else if (sum == 2 || sum == 12)
 			{
-				System.out.print("I'm sorry but you lose! Would you like to play again? (y/n)");
+				System.out.print("\nI'm sorry but you lose!");
 				
 			}
 			else
-			{
+			{	//sets point
+				myPoint = sum;
 				System.out.print("\nYour point is: "  + myPoint);
-			}
-			
-			//roll until you get your point or 7(lose)
-			while(sum != 7 || sum != myPoint)
-			{
+				
 				System.out.print("\nPress enter to roll again");
 				in.nextLine();
-				roll1 = die1.roll();
-				roll2 = die2.roll();
-				sum = roll1 + roll2;
+				sum = (die1.roll()  + die2.roll());
 				System.out.print("Die  1: " + roll1 +  "\tDie 2: " + roll2 + "\tSum: " + sum);	
-			}
-			
-			//if 7 lose
-			if (sum == 7)
-			{
-				System.out.println("You loose!");
-			}
-			else if (sum == myPoint) //win
-			{
-				System.out.println("You win!");
-			}
+
+				//roll until you get your point or 7(lose)
+				while(sum != 7 && sum != myPoint)
+				{
+					System.out.print("\nPress enter to roll again");
+					in.nextLine();
+					roll1 = die1.roll();
+					roll2 = die2.roll();
+					sum = roll1 + roll2;
+					System.out.print("Die  1: " + roll1 +  "\tDie 2: " + roll2 + "\tSum: " + sum);	
+				}
+				//if 7 lose
+				if (sum == 7)
+				{
+					System.out.println("\nYou loose!");
+				}
+				else if (sum == myPoint) //win
+				{
+					System.out.println("\nYou win!");
+				}
 		}
-		
-		
-		
 		//playing again
 		String again = "";
-		System.out.print("Would you like to play again? (y/n)");
+		System.out.print("\nWould you like to play again? (y/n)");
 		again = in.nextLine();
-			while(again != "y" || again != "n")
+		
+		//double checks if its a correct value
+			while(again.equals("y") == false && again.equals("n") == false)
 			{
 				System.out.print("That is not an option. Would you like to play again? (y/n)");
 				again = in.nextLine();
 			}
-		if (again == "y")
+		if (again.equals("y"))
 		{
 			play = true;
 		}
 		else
 		{
 			play = false;
+			System.out.println("Thank you for playing!");
 		}
 		
+		}
 	}
 }
