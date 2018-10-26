ee4b1f60d215660b17dcb3cd98b95f5e58457c49
khristova21 <42977135+khristova21@users.noreply.github.com>
2018-10-26 13:36:02 -0400

    Add files via upload

---
+++ DiceArray.java
@@ -0,0 +1,25 @@
+/**
+ *Dice Array
+ * @author Krisi Hristova
+ * Pd.6
+ *
+ */
+public class DiceArray {
+
+	public static void main(String[] args)
+	{
+		//create and roll 2 dice
+		//use an array with 13 slots to have its index equal to the number 2-12
+		//roll 1000 times (for loop)
+		//count and display number of times each  sum 2-12 was rolled (store each in one variable array with 13 slots)
+		
+		Dice die1 = new Dice();
+		Dice die2 = new Dice();
+		
+		for(int i = 0; //restriction   //increment)
+				{
+					
+				}
+	}
+
+}
